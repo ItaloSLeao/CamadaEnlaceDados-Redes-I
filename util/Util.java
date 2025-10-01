6275e20000d022d7917b209c8771918aebe6256a
ItaloSLeao <italoleao200656@gmail.com>
2025-10-01 16:11:42 -0300

    refactor: Util.java

--- util/Util.java
+++ util/Util.java
@@ -7,7 +7,7 @@ package util;
  * na interface grafica, utilizadas pelas camadas de enlace e pelo meio de comunicacao.
  *
  * @author  Italo de Souza Leao (Matricula: 202410120)
- * @version 27/09/2025 (Ultima alteracao)
+ * @version 01/10/2025 (Ultima alteracao)
  * @since   26/08/2025 (Inicio)
  */
 public class Util {
@@ -24,22 +24,22 @@ public class Util {
    */
   public static String bitsParaString(int bits) {
     String retorno = "";
-    int mascara = 1;
-
+    int mascara = 1; //A mascara comeca no primeiro bit (LSB)
+  
     for (int i = 0; i < 32; i++) {
-      if (i % 8 == 0){ //A cada 8 bits da um espaco de separacao visual
+      if (i > 0 && i % 8 == 0) { //Adiciona espaco a cada 8 bits (exceto no inicio)
         retorno = ' ' + retorno;
-      } //Fim if
-      
-      mascara <<= 1; //Move a mascara 1 bit a esquerda
-      
-      if ((mascara & bits) != 0) //Se o primeiro bit do inteiro nao for 0
+      } 
+      //Correcao de formatacao: Primeiro verifica, depois desloca.
+      if ((mascara & bits) != 0) { //Verifica o bit na posicao atual da mascara
         retorno = "1" + retorno;
-      else
+      } else {
         retorno = "0" + retorno;
-    } //Fim for
-
-    return retorno;
+      } //Fim if-else
+      mascara <<= 1; //Move a mascara para a proxima posicao a esquerda
+    }
+  
+    return retorno.trim(); //.trim() para remover o espaco extra no comeco
   } //Fim bitsParaString
 
 } //Fim da classe Util
